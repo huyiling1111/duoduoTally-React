5c5a590d2e7982cd2e19583a2dbed0dc7c0caa1b
huyiling1111 <1052859577@qq.com>
2021-02-17 15:19:42 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/detail">detail</Link>
+            </li>
+            <li>
+              <Link to="/chart">chart</Link>
+            </li>
+            <li>
+              <Link to="/money">money</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/detail">
+            <Home />
+          </Route>
+          <Route path="/chart">
+            <About />
+          </Route>
+          <Route path="/money">
+            <Users />
+          </Route>
+          <Route exact from="/">
+            <Redirect to="/detail" />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+function NoMatch() {
+  return (
+    <div>
+      <h3>找不到</h3>
     </div>
   );
 }
